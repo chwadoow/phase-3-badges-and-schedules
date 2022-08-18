251645c7c5e457c76b6165512a82aa9be02ec031
moses ngige <moses32ngigz@gmail.com>
2022-08-18 17:13:04 +0300

    commit tree 1d7b51a510d9faf3a9b5ff263741b9b8f11e8eb1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator param
+  
+    param.map do |x|
+        "Hello, my name is #{x}."
+    end 
+end
+
+def assign_rooms param 
+    rooms = 0
+    param.map{|x|
+    rooms += 1
+    "Hello, #{x}! You'll be assigned to room #{rooms}!"
+}   
+end
+def printer param
+    param.each do |x|
+        puts batch_badge_creator (x)    
+    end
+    param.each do |x|
+        puts assign_rooms (x)
+    end
+    
+
+end
